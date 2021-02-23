2f297ae05f2c15024c5c821b02313dd6c4435089
1023150697@qq.com <1023150697@qq.com>
2021-02-23 17:57:29 +0800

    改了点东西

--- src/store/index.js
+++ src/store/index.js
@@ -7,15 +7,23 @@ export default new Vuex.Store({
   state: {
     // 登陆中
     isLogining: false,
-    zUser: JSON.parse(localStorage.getItem('zUser')) || {}
+    zUser: JSON.parse(localStorage.getItem('zUser')) || {},
+    isLogin: !!localStorage.getItem('zUser')
   },
   mutations: {
     setIsLoginingHandle (state, value) {
       console.log(value)
       state.isLogining = value
     },
+    // 设置用户信息 用户登入 登出
     setuserInfo (state, v) {
-      localStorage.setItem('zUser', JSON.stringify(v))// 将传递的数据先保存到localStorage中
+      if (v === '') {
+        localStorage.removeItem('zUser')// 将传递的数据先保存到localStorage中
+        state.isLogin = false
+      } else {
+        state.isLogin = true
+        localStorage.setItem('zUser', JSON.stringify(v))// 将传递的数据先保存到localStorage中
+      }
       state.zUser = v// 之后才是修改state中的状态
     },
     addmu (state) { state.count++ },
